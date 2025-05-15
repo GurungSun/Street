812e310d5f4835c1e5dd9120dcb930074c05bdf4
GurungSun <sugurung@s6c.mlp.college>
2025-05-15 13:22:45 +0100

    added app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask,render_template
+PORT= 2100
+
+app = Flask(__name__,template_folder='template')
+@app.route("/") 
+def index():
+    return render_template()
+if __name__ == "__main__": 
+	app.run(debug=True, host = '0.0.0.0', port=PORT) 
+	app.register_blueprint("home_bp")
