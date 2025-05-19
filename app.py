1c05753219897ab377efbaf6f6b31c0a28690674
GurungSun <sugurung@s6c.mlp.college>
2025-05-19 11:51:36 +0100

    update

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask,render_template
-PORT= 2100
+from flask import Flask,render_template, url_for
+from arts import art
 
-app = Flask(__name__,template_folder='template')
+app = Flask(__name__)
+
 @app.route("/") 
 def index():
-    return render_template()
+    return render_template("index.html", arts=art)
+
 if __name__ == "__main__": 
-	app.run(debug=True, host = '0.0.0.0', port=PORT) 
-	app.register_blueprint("home_bp")
+	app.run(debug=True, host = '0.0.0.0', port=5100)
---
+++ arts.py
@@ -0,0 +1,6 @@
+art = [
+    {
+        "id": 1,
+        "title": "What is Street Art"
+    },
+]
